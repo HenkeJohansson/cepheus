d7b6ad40e06268e3318e323f2ae49428173c2667
Henrik Johansson <henrik@bastakompisar.se>
2016-10-03 01:20:52 +0200

    added and testing some functions

--- app/js/app.js
+++ app/js/app.js
@@ -7,7 +7,26 @@
 	var $terminalWindow = $terminal.find('.window');
 	var $terminalInput = $terminal.find('input.comand');
 	var compName = '[mainframe] ~ $ ';
-	var commandsHistory = [];
+	var windowHistory = [];
+	var commandHistory = [];
+	var subDir = [
+		{
+			name: 'projects',
+			type: 'folder'
+		},
+		{
+			name: 'info',
+			type: 'folder'
+		},
+		{
+			name: 'cv',
+			type: 'folder'
+		},
+		{
+			name: 'avatar.jpg',
+			type: 'image'
+		}
+	];
 
 	$terminal.on('click', focusInput);
 
@@ -18,17 +37,61 @@
 
 	function render() {
 		$terminalWindow.html('');
-		$.each(commandsHistory, function() {
+		$.each(windowHistory, function() {
 			$terminalWindow.append(this);
 		});
 	}
 
 	function command(e) {
 		if ( e.keyCode === 13 ) {
-			console.log('input', e);
+			storeComando();
 			updateWindow();
+			findFunc();
 			$terminalInput.val('');
 		}
+		if ( e.keyCode === 38 ) {
+			var commandIdx = commandHistory.length-1;
+			cyclePrevComandos(commandIdx);
+		}
+	}
+
+	function findFunc() {
+		var command = $terminalInput.val();
+		command = command.split(' ');
+		var func = command[0];
+		var value = command[1];
+		if ( func === 'ls' ) {
+			listDirs();
+		} else if ( func === 'cd' ) {
+			changeDir(value);
+		}
+	}
+
+	function listDirs() {
+		$.each(subDir, function() {
+			var commandLine = $('<p>', {
+				html: this.name,
+				class: 'ls'
+			});
+			windowHistory.push(commandLine);
+		});
+		render();
+	}
+
+	function changeDir(value) {
+		/**
+		 * dunno
+		 */
+
+	}
+
+	function storeComando() {
+		commandHistory.push($terminalInput.val());
+	}
+
+	function cyclePrevComandos(commandIdx) {
+		var commando = commandHistory[commandIdx];
+		$terminalInput.val(commando);
 	}
 
 	function updateWindow() {
@@ -36,7 +99,7 @@
 		var commandLine = $('<p>', {
 			html: command
 		});
-		commandsHistory.push(commandLine);
+		windowHistory.push(commandLine);
 		render();
 	}
 
