8d6acef24abb6431c4bd0e80e323f0f68e1273c9
Henrik Johansson <henrik@bastakompisar.se>
2016-10-02 21:07:44 +0200

    window now gets command-history, and input values disapears

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,49 @@
-(function() {
+(function($) {
 	'use strict';
-	// App.js
-})();
+	
+	var $wrapper = $('.wrapper');
+	var $terminal = $wrapper.find('.terminal');
+	var $topbar = $terminal.find('.top-bar');
+	var $terminalWindow = $terminal.find('.window');
+	var $terminalInput = $terminal.find('input.comand');
+	var compName = '[mainframe] ~ $ ';
+	var commandsHistory = [];
+
+	$terminal.on('click', focusInput);
+
+	$terminalInput.on('keyup', function(e) {
+		command(e);
+	});
+
+
+	function render() {
+		$terminalWindow.html('');
+		$.each(commandsHistory, function() {
+			$terminalWindow.append(this);
+		});
+	}
+
+	function command(e) {
+		if ( e.keyCode === 13 ) {
+			console.log('input', e);
+			updateWindow();
+			$terminalInput.val('');
+		}
+	}
+
+	function updateWindow() {
+		var command = compName + $terminalInput.val();
+		var commandLine = $('<p>', {
+			html: command
+		});
+		commandsHistory.push(commandLine);
+		render();
+	}
+
+	function focusInput() {
+		$terminalInput.focus();
+	}
+
+
+
+})(jQuery);
